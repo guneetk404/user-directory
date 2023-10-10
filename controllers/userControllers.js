8d85ca7126ff9654dd8fadf3ce2cf98abf2ab767
guneets <guneets@argusoft.com>
2023-10-10 11:25:48 +0530

    completed google auth

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,7 +1,7 @@
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-
+const axios = require("axios");
 module.exports.signup = async (req, res) => {
   try {
     console.log(req.body);
@@ -39,20 +39,19 @@ module.exports.login = async (req, res) => {
 
     if (!user) {
       return res.status(401).send({
-        message:"sorry Wrong Credentials"
-
+        message: "sorry Wrong Credentials",
       });
     }
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res.status(401).send({
-        message:"sorry Wrong Credentials"
+        message: "sorry Wrong Credentials",
       });
     }
     const jwtKey = process.env.JWT_SECRET;
     delete user.password;
-    const userEmail= user.toObject().email
-    const token = jwt.sign({userEmail}, jwtKey, { expiresIn: "2d" });
+    const userEmail = user.toObject().email;
+    const token = jwt.sign({ userEmail }, jwtKey, { expiresIn: "2d" });
     // console.log(token);
     res.status(200).send({
       email: user.email,
@@ -70,22 +69,20 @@ module.exports.login = async (req, res) => {
 module.exports.updateUser = async (req, res) => {
   try {
     const email = req.body.email;
-    console.log("email inside update",email)
+    console.log("email inside update", email);
     const updatedUserData = req.body;
     console.log(req.body);
     console.log(req.tokendata);
-    if(email===req.tokendata.userEmail){ 
-      const user = await User.findOneAndUpdate({email},updatedUserData);
+    if (email === req.tokendata.userEmail) {
+      const user = await User.findOneAndUpdate({ email }, updatedUserData);
       if (!user) {
         console.log("Error finding the user");
       }
       return res.status(200).send("user updated successfully");
-    }else{
-      return res.status(401).end("Unauthorized Access");  
+    } else {
+      return res.status(401).end("Unauthorized Access");
     }
-    
-  } 
-  catch (err) {
+  } catch (err) {
     console.log("error", err);
     return res.status(500).end("Internal Server Error");
   }
@@ -93,11 +90,11 @@ module.exports.updateUser = async (req, res) => {
 
 module.exports.userFetcher = async (req, res) => {
   try {
-    const email =req.params.id
-    const results =   await User.findOne({ email }, { password: 0 });
-    console.log("trying")
-    console.log("userFetcher",results)
-    delete results.password
+    const email = req.params.id;
+    const results = await User.findOne({ email }, { password: 0 });
+    console.log("trying");
+    console.log("userFetcher", results);
+    delete results.password;
     return res.status(200).json({
       message: "Success",
       body: results,
@@ -111,49 +108,43 @@ module.exports.userFetcher = async (req, res) => {
   }
 };
 
-
-
 module.exports.googleLogin = async (req, res) => {
   try {
     console.log(req.body.data.email);
-    const email = req.body.email;
+    const email = req.body.data.email;
     const user = await User.findOne({ email });
     if (!user) {
       return res.status(401).send({
-        message:"sorry Wrong Credentials"
-
+        message: "sorry Wrong Credentials",
       });
     }
 
     const accessToken = req.body.data.accessToken;
     const clientId = req.body.data.clientId;
-    const googleResponse = await axios.get(`https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${accessToken}`)
-    console.log(googleResponse);
+    console.log("at", accessToken);
+
+    const googleResponse = await axios.get(
+      `https://oauth2.googleapis.com/tokeninfo?id_token=${accessToken}`
+    );
+    //  console.log(googleResponse);
     if (googleResponse.data.aud === clientId) {
-      // Token is valid, you can trust it.
-      // You can access user information from googleResponse.data.
-      // Perform the necessary actions or grant access based on your application's requirements.
       console.log("done");
-      res.status(200).json({ message: 'Authentication successful' });
+      const jwtKey = process.env.JWT_SECRET;
+      const userEmail = user.toObject().email;
+      const token = jwt.sign({ userEmail }, jwtKey, { expiresIn: "2d" });
+      res.status(200).json({
+        email: user.email,
+        name: user.name,
+        token: token,
+        message: "You have been successfully logged in",
+        success: true,
+      });
     } else {
-      // The token is not valid or doesn't belong to your app.
-      console.log("not done")
-      res.status(401).json({ message: 'Invalid access token' });
+      console.log("not done");
+      res.status(401).json({ message: "Invalid access token" });
     }
-    const jwtKey = process.env.JWT_SECRET;
-    delete user.password;
-    const userEmail= user.toObject().email
-    const token = jwt.sign({userEmail}, jwtKey, { expiresIn: "2d" });
-    // console.log(token);
-    res.status(200).send({
-      email: user.email,
-      name: user.name,
-      token: token,
-      message: "You have been successfully logged in",
-      success: true,
-    });
   } catch (error) {
-    console.log(error);
+    //console.log(error);
     return res.status(500).end("Internal Server Error bye bye");
   }
 };
